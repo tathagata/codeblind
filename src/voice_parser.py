4138f247eff172b96e65fe86b3390f1116e8a963
Tathagata <tathagatadg@gmail.com>
2026-03-01 00:22:18 -0500

    Add VoiceParser class for Voice-to-Intent Parsing module

---
+++ src/voice_parser.py
@@ -0,0 +1,13 @@
+class VoiceParser:
+    def __init__(self):
+        pass
+
+    def speech_to_text(self):
+        # Pseudo-code for speech-to-text using macOS APIs
+        converted_text = ""  # Imagine this is where we process and get the speech input
+        print(converted_text)
+
+# Example usage
+if __name__ == '__main__':
+    parser = VoiceParser()
+    parser.speech_to_text()
